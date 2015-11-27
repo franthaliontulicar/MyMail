d61e7821e4f761f9ab2198f07b611dd2317c0d27
fran <franmenendezpouso@gmail.com>
2015-11-27 13:33:11 +0100

    controlar el ultimo  mensaje recibido

--- MailClient.java
+++ MailClient.java
@@ -10,25 +10,32 @@ public class MailClient
     // instance variables - replace the example below with your own
     private MailServer server;
     private String user;
-    
+    private MailItem ultimoMensaje;
+
     public MailClient(MailServer server, String user){
 
         this.server = server;
         this.user = user;
+        ultimoMensaje = null;
 
     }
 
     public MailItem getNextMailItem(){
-
-        return server.getNextMailItem(user);
+        MailItem correo = null;
+        if(server.howManyMailItems(user) >0){
+            ultimoMensaje = server.getNextMailItem(user);
+            correo = ultimoMensaje;
+        }
+        return correo;
 
     }
+
     public void printNextMailItem(){
         MailItem mensaje = getNextMailItem();
-       
+
         if(mensaje != null){
             mensaje.print();
-           
+
         }
         else{
             System.out.println("No tiene mensajes");
@@ -40,18 +47,27 @@ public class MailClient
         MailItem mensaje = new MailItem(user,to,mensajeEnviado,asunto);
         server.howManyMailItems(user);
         server.post(mensaje);
-        
 
     }
-    
-    public void getNextMailItemAndAutorespond(String to, String mensajeEnviado, String asunto){
+    public MailItem  getNextMailItemAndAutorespond(String to, String mensajeEnviado, String asunto){
         MailItem mensaje = new MailItem(user,to,mensajeEnviado,asunto);
         server.getNextMailItem(user);
         server.post(mensaje);
         mensaje.print();
-        System.out.println("Re " + asunto + " Sentimos no poder atenderle, estamos de vacaciones ");
-       
-      
+        System.out.println("Re " + asunto);
+        System.out.println("Sentimos no poder atenderle, estamos de vacaciones ");
+        return mensaje;
+
+    }
+
+    public void ultimoMensajeRecibido(){
+        if(ultimoMensaje != null){  
+             ultimoMensaje.print();
+        }
+        else{
+            System.out.println("No hay mensajes en su bandeja ");
+        
+        }
+
     }
 }
-
